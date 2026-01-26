a4c22c8b847c365264fe3c774d972a55aba0d64a
Anton Norman <normananton03@gmail.com>
2026-01-26 23:49:52 +0100

    chore: Separating auth to it's own module

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,10 @@
-use serde::{Deserialize, Serialize};
-use serde_json;
-use std::env;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::PathBuf;
-
-#[derive(Debug, Serialize, Deserialize)]
-struct UserInformation {
-    name: String,
-    surname: String,
-    email: String,
-    auth_token: String,
-}
-
-fn get_local_information() -> Result<UserInformation, Box<dyn std::error::Error>> {
-    let home_dir = match env::home_dir() {
-        Some(path) => path,
-        None => PathBuf::new(),
-    };
-
-    let home_str: &str = home_dir.to_str().unwrap();
-    let full_path = String::from(home_str) + "/.passport/authentication.json";
-
-    let file: File = File::open(full_path)?;
-    let reader = BufReader::new(file);
-    let user: UserInformation = serde_json::from_reader(reader)?;
-    Ok(user)
-}
+mod auth;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let local_user = match get_local_information() {
+    let local_user = match auth::get_local_information() {
         Ok(person) => person,
         Err(e) => return Err(e),
     };
-    println!(
-        "AuthToken: {}\nName: {} {}\nEmail: {}",
-        local_user.auth_token, local_user.name, local_user.surname, local_user.email
-    );
+    println!("{}", local_user);
     Ok(())
 }
